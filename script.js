f18e1d7a604b8930fdc4154869a82cd1fdae7fc3
Gayatri Budha <gayatribudha5@gmail.com>
2023-02-14 00:46:47 +0545

    Add custom css

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// const images = [
-//   "images/img111.webp",
-//   "images/img222.jpg",
-//   "images/img333.jpg",
-//   "images/img444.jpg",
-//   "images/img555.jpg",
-//   "images/img777.jpg",
-//   "images/img66.webp",
-//   "images/img77.webp",
-// ];
-
 const images = [
   "images/img22.jpg",
   "images/img1.jpg",
@@ -20,6 +9,5 @@ const images = [
 ];
 
 let wrapper = document.querySelector(".container");
-let wrapper2 = document.querySelector(".container2");
 
 let carousel1 = new Carousel(images, wrapper);
